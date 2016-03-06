e43b1f204933c95d165b8ab07d212489ec8696cc
Wilbur Yang <wilbury@stanford.edu>
2016-03-06 15:13:05 -0800

    Does something sometimes, at least

--- siam_net.py
+++ siam_net.py
@@ -2,6 +2,7 @@ import keras as k
 from data_utils import *
 from keras.models import Sequential
 from keras.layers.core import *
+from keras.layers.normalization import BatchNormalization
 from keras import backend as K
 
 def reshapeArray(arr):
@@ -33,7 +34,8 @@ def chopra_loss(y_true, y_pred):
     ''' (1-Y)(2/Q)(Ew)^2 + Y2Qe^(-2.77/Q*Ew) '''
     # Is something wrong here? loss is nan
     Q = 10.
-    return (1 - y_true) * 2 / Q * (y_pred ** 2) + y_true * 2 * Q * K.exp(-2.77 / Q * y_pred)
+    #return y_true - y_pred
+    return (1 - y_true) * 2 / Q * K.square(y_pred) + y_true * 2 * Q * K.exp(-2.77 / Q * y_pred)
 
 print 'Getting CIFAR10 data...'
 data = get_CIFAR10_data()
@@ -64,6 +66,8 @@ right_model.add(Lambda(identity, input_shape=(input_dim,)))
 
 # Add 6 dense layers (varying the hidden dimensions gives me errors?)
 add_shared_layer(Dense(output_dim=10, input_dim=input_dim, init=init), inputs)
+left_model.add(BatchNormalization(epsilon=1e-06, mode=0, axis=-1, momentum=0.9, weights=None))
+right_model.add(BatchNormalization(epsilon=1e-06, mode=0, axis=-1, momentum=0.9, weights=None))
 left_model.add(Activation('relu'))
 right_model.add(Activation('relu'))
 
@@ -78,21 +82,42 @@ model = Sequential()
 def l2dist(x):
     ''' x[0] is the output from left
         x[1] is the output from right '''
-    z = x[0] - x[1]
-    return K.sqrt(K.sum(K.square(z), keepdims=True))
+    #z = x[0] - x[1]
+    #return K.sqrt(K.sum(K.square(z), keepdims=True))
+    import theano.tensor as T
+    return T.sqrt((T.sqr(x[0] - x[1])).sum(axis=1, keepdims=True))
+
+
 model.add(LambdaMerge([left_model, right_model], function=l2dist))
+#model.add(Merge([left_model, right_model], mode='mul'))
 
 print 'Compiling model...'
 model.compile(loss=chopra_loss, optimizer='adam')
 
 print 'Fitting model...'
-model.fit([x_train, x_train], np.zeros_like(y_train), nb_epoch=5, batch_size=64)
+#model.fit([x_train, x_train], np.zeros_like(y_train), nb_epoch=5, batch_size=64)
+
+def generate_impostor_data(x, y):
+    r = np.arange(x.shape[0])
+    left_ind, right_ind = np.random.permutation(r), np.random.permutation(r)
+    return x[left_ind,:], x[right_ind,:], (np.argmax(y, axis=1) == np.argmax(y, axis=1))
+
+left_perm, right_perm, y = generate_impostor_data(x_train, y_train)
+y = np.concatenate((np.zeros((y_train.shape[0],)), y), axis=0)
+y.reshape(98000, 1)
+print y
+model.fit([np.concatenate((x_train, left_perm), axis=0), np.concatenate((x_train, right_perm), axis=0)],
+        y, nb_epoch=5, batch_size=64)
 
 print 'Validating model...'
-val_loss = model.evaluate([x_val, x_val], np.zeros_like(y_val), batch_size=1)
+val_loss = model.evaluate([x_train, x_train], np.zeros_like(y_train), batch_size=1)
 print 'validation loss: %f' % val_loss
-preds = model.predict_classes([x_val, x_val], batch_size=1)
+#left_perm, right_perm, y = generate_impostor_data(x_val, y_val)
+#preds = model.predict_classes([np.concatenate((x_val, left_perm), axis=0), np.concatenate((x_val, right_perm), axis=0)], batch_size=1)
+#preds = model.predict_classes([left_perm, right_perm], batch_size=1)
+preds = model.predict_classes([x_train, x_train], batch_size=1)
 print preds
+print preds.sum()
 #acc = [1 for i,p in enumerate(preds) if np.argmax(p) == np.argmax(data['y_val'][i])]
 #acc = np.array(acc).sum() / preds.shape[0]
 #print('Accuracy: %f' % acc)
